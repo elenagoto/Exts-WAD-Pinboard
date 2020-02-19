a9fae8d6105c1f55a74257aadddfea7c5a9786ad
Laura Gomez <laura@elenagoto.ch>
2020-02-19 18:33:28 +0100

    Replace double quotes with single quotes in home test

--- test/system/homes_test.rb
+++ test/system/homes_test.rb
@@ -1,8 +1,8 @@
 require "application_system_test_case"
 
 class HomesTest < ApplicationSystemTestCase
-  test "visiting the index" do
+  test 'visiting the index' do
     visit '/'
-    assert_selector "h1", text: "Add, pin and share your favorite images"
+    assert_selector 'h1', text: 'Add, pin and share your favorite images'
   end
 end

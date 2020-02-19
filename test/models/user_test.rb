b57c75bf65db404001a8119dc8b1ffca7ba4a3be
Laura Gomez <laura@elenagoto.ch>
2020-02-19 20:26:53 +0100

    Add Model test to add new idea and test email required

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Create new user' do
+    user = User.new email: 'happy@home.ch'
+    user.save!
+
+    assert_equal User.first, user
+    assert_equal User.all.length, 1
+  end
+
+  test 'Email is required' do
+    user = User.new
+    refute user.valid?
+  end
 end

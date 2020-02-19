f74d49f406eca7557baf1dd24518806a61330c58
Laura Gomez <laura@elenagoto.ch>
2020-02-19 19:59:22 +0100

    Add private params to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,10 @@ class UsersController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
 end

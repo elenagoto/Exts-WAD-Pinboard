5b18d9bf8e7d5a925825c9f410abd697f9dc5bac
Laura Gomez <laura@elenagoto.ch>
2020-02-20 18:32:54 +0100

    Add routes for Pins and Images

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # Resources
-  resources :users
+  resources :users do
+    resources :images
+  end
+
+  resources :pins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

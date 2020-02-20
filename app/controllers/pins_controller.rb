0f42f28eabd2d42b82f18ed8f17b168ee03827df
Laura Gomez <laura@elenagoto.ch>
2020-02-20 20:55:17 +0100

    Add pin#New view with form and controllers to create a new pin Modify button in user#images view to redirect to new pin

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,2 +1,27 @@
 class PinsController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.find(session[:user_id])
+    @pin = Pin.new
+  end
+
+  def create
+    user = User.find(session[:user_id])
+    @pin = Pin.new(pin_params)
+    @pin.user = user
+
+    if @pin.save
+      redirect_to user_images_path(user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :image_url, :tag)
+  end
 end

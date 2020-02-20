60717d25b03b9710e1d899ea30c5fd73a2318538
Laura Gomez <laura@elenagoto.ch>
2020-02-20 18:14:25 +0100

    Add relationships to User and Pin models and validations to Pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,9 @@
 class Pin < ApplicationRecord
+  # Validation rules
+  validates :title, presence: true
+  validates :tag, length: { maximum: 30 }
+
+  # Relationships
   belongs_to :user
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   # Validation rules
   validates :email, presence: true
+
+  # Relationships
+  has_many :pins
+  has_and_belongs_to_many :images, class_name: 'Pin'
 end

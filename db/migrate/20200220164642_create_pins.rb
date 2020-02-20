7276219c320a5e43e8752ead9c4d89a6ee3c65a5
Laura Gomez <laura@elenagoto.ch>
2020-02-20 17:47:17 +0100

    Generate Pin model with title, image_url, tag and user as attributes

---
+++ db/migrate/20200220164642_create_pins.rb
@@ -0,0 +1,12 @@
+class CreatePins < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pins do |t|
+      t.string :title
+      t.string :image_url
+      t.string :tag
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200219184553) do
+ActiveRecord::Schema.define(version: 20200220164642) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "title"
+    t.string "image_url"
+    t.string "tag"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pins_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"

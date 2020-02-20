342bd6cd4aa41ad04fc700190d7d1116954e3321
Laura Gomez <laura@elenagoto.ch>
2020-02-20 19:17:22 +0100

    Create Model test for creating and validating Pins

--- test/models/pin_test.rb
+++ test/models/pin_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class PinTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Create new pin' do
+    pin = Pin.new user: User.new,
+                  title: 'This is a title for idea'
+    pin.save!
+
+    assert_equal Pin.first, pin
+    assert_equal Pin.all.length, 1
+  end
+
+  test 'validation for pin without a title' do
+    pin = Pin.new user: User.new
+    refute pin.valid?
+  end
+
+  test 'Validation for tag too long' do
+    pin = Pin.new user: User.new,
+                  title: 'This is a title for idea',
+                  tag: 'This is a very long description of the pin I want to create'
+    refute pin.valid?
+  end
 end

a2b2076188f286873a3dd9f00feec902190259a4
Laura Gomez <laura@elenagoto.ch>
2020-02-21 17:48:14 +0100

    Activate Pin button in card to add to User's images list

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,11 @@ class ImagesController < ApplicationController
   def index
     @user = User.find(session[:user_id])
   end
+
+  def create
+    @user = User.find(session[:user_id])
+    @pin = Pin.find(params[:pin_id])
+    @user.images << @pin
+    redirect_back(fallback_location: root_path)
+  end
 end

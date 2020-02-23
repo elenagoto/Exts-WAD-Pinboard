6a776ccf82c6260b25fa843c86b3bfb06f3ce59a
Laura Gomez <laura@elenagoto.ch>
2020-02-23 13:15:14 +0100

    Add system tests to differnt content when user logged in

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -18,4 +18,32 @@ class UsersTest < ApplicationSystemTestCase
     assert_equal User.all.length, 0
     assert page.has_content? "Email can't be blank"
   end
+
+  test 'user can see My Pins when logged' do
+    visit root_path
+    assert page.has_content? 'Login/Register'
+    click_on 'Login/Register', match: :first
+
+    fill_in 'Email', with: 'happy@home.ch'
+    click_on 'Log in', match: :first
+
+    assert page.has_content? 'My Pins'
+    refute page.has_content? 'Login/Register'
+  end
+
+  test 'users can edit pins when logged' do
+    user = User.new email: 'happy@home.ch'
+    pin = Pin.new title: 'Unedited pin', user: user
+    pin.save!
+
+    visit pin_path(pin)
+    refute page.has_content? 'Edit'
+
+    click_on 'Login/Register', match: :first
+    fill_in 'Email', with: user.email
+    click_on 'Log in', match: :first
+
+    visit pin_path(pin)
+    assert page.has_content? 'edit'
+  end
 end

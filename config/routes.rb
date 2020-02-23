075787089b871a9958e5bf28361867b90b816c88
Laura Gomez <laura@elenagoto.ch>
2020-02-23 15:59:59 +0100

    Add comments routes associated with pins routes Update app_routes text document

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
     resources :images
   end
 
-  resources :pins
+  resources :pins do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

89981ae167fb2f6c791e3e09580bc206187617b1
Laura Gomez <laura@elenagoto.ch>
2020-02-23 18:49:49 +0100

    Add system test to add and display comments in pin#show view

--- test/system/comments_test.rb
+++ test/system/comments_test.rb
@@ -1,9 +1,30 @@
 require "application_system_test_case"
 
 class CommentsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit comments_url
-  #
-  #   assert_selector "h1", text: "Comment"
-  # end
+  test 'adding a Comment to a Pin' do
+    user = User.new email: 'happy@home.ch'
+    user.save!
+
+    pin = Pin.new title: 'This is Pin title', user: user
+    pin.save!
+
+    visit new_user_path
+    fill_in 'Email', with: user.email
+    click_on 'Log in', match: :first
+
+    visit pin_path(pin)
+    fill_in 'Add a comment', with: 'Beautiful image'
+    click_on 'Post'
+
+    assert_equal current_path, pin_path(pin)
+    assert page.has_content? 'Beautiful image'
+  end
+
+  test 'comments cannot be added when not logged in' do
+    pin = Pin.new title: 'This is Pin title', user: User.new
+    pin.save!
+
+    visit pin_path(pin)
+    refute page.has_content? 'Add a comment'
+  end
 end

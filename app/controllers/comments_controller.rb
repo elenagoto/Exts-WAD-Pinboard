d252338e79e0e8e831e58996511188e7be636432
Laura Gomez <laura@elenagoto.ch>
2020-02-23 17:51:33 +0100

    Add Create methos to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    user = User.find(session[:user_id])
+    pin = Pin.find(params[:pin_id])
+    comment = Comment.new(comment_params)
+    comment.user = user
+    comment.pin = pin
+    comment.save!
+    redirect_to pin_path(pin)
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
